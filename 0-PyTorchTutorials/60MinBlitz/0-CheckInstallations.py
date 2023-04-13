03681a51c475ef35c884ba127dcc7fde6bac6ce9
mapfeiff <mapfeiff@uci.edu>
2023-04-12 22:43:52 -0700

    Initial Commit: Added repository structure for future files and created Read Me files

---
+++ 0-PyTorchTutorials/60MinBlitz/0-CheckInstallations.py
@@ -0,0 +1,5 @@
+#Try to import the PyTorch and NumPy libraries
+#Both libraries should have been installed using the command: pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu117
+#If any errors, more information on importing PyTorch is found on their website: https://pytorch.org/get-started/locally/
+import torch
+import numpy
---
+++ 0-PyTorchTutorials/ReadMe.md
@@ -0,0 +1,1 @@
+# This is a repository consisting of all code written while following PyTorch tutorials
