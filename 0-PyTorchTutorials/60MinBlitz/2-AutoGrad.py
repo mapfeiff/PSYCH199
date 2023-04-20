307fb5904a00a0efd72ec25da5a94751a804c38c
mapfeiff <mapfeiff@uci.edu>
2023-04-19 20:53:25 -0700

    Started second page tutorial on AutoGrad

---
+++ 0-PyTorchTutorials/60MinBlitz/2-AutoGrad.py
@@ -0,0 +1,11 @@
+"""
+AutoGrad: Learch about PyTorch's automatic differentiation tool
+Tutorial Page: https://pytorch.org/tutorials/beginner/blitz/autograd_tutorial.html
+"""
+
+import torch
+from torchvision.models import resnet18, ResNet18_Weights
+
+#=========================
+# Usage in PyTorch
+#=========================
